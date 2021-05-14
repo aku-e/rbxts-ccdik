d70d01b1b3a5a4b6c80b242259e4aeb5cfcaac9a
aku <68713456+aku-e@users.noreply.github.com>
2021-05-15 00:23:39 +0300

    initial commit

---
+++ src/index.d.ts
@@ -0,0 +1,28 @@
+declare const ConstraintsMap: Map<Motor6D, Map<string, unknown>>;
+
+type CCDIKController = {
+	Constraints: typeof ConstraintsMap | undefined;
+	LerpMode: boolean;
+	LerpAlpha: number;
+	ConstantLerpSpeed: number;
+	AngularSpeed: number;
+
+	GetConstraints(): void;
+	GetConstraintsFromMotor(Motor6D: Motor6D, constraintName: string): void;
+	CCDIKIterateOnce(goalPosition: Vector3, tolerance: number | undefined, step: number): void;
+	CCDIKIterateUntil(
+		goalPosition: Vector3,
+		tolerance: number | undefined,
+		maxBreakCount: number | undefined,
+		step: number,
+	): void;
+	SetupFoot(attachmentNameTable: Array<string>, raycastParams: RaycastParams): void;
+	InitDragDebug(): void;
+};
+
+interface CCDIKConstructor {
+	new (Motor6DArray: Array<Motor6D>, ConstraintsDictionary?: typeof ConstraintsMap): CCDIKController;
+}
+declare const CCDIKController: CCDIKConstructor;
+
+export = CCDIKController;
